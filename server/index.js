202fad45e507c4141febccbdf62356e2c10afec5
Rio0628 <mariomdomenech@gmail.com>
2021-12-01 23:21:18 -0500

    Added router
    - Added the router for handling the exchange of information between the database and the application.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
+const sessionRouter = require('./routes/session-router')
 const app = express();
 const apiPort = 3000;
 
@@ -11,5 +12,6 @@ app.use(express.json());
 db.on('error', console.error.bind(console, 'MongoDB Connection Error:'))
 
 app.get('/', (req, res) => { res.send('Hello World!') })
+app.use('/api', sessionRouter);
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`)) 
---
+++ server/routes/session-router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const SessionCtrl = require('../controllers/session-cntrl');
+const router = express.Router();
+
+router.post('/session', SessionCtrl.createSession);
+router.delete('/session/:id', SessionCtrl.deleteSession);
+router.get('/session/:id', SessionCtrl.getSessionById);
+router.get('/sessions', SessionCtrl.getSessions);
+
+module.exports = router;
