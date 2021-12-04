6fadf3ed742e55555a58f9d499eee4100c95803a
Rio0628 <mariomdomenech@gmail.com>
2021-12-03 23:41:08 -0500

    Added Functionality
    - Implemented the react router dom completely into the application.
    - Now when the user clicks one of the navbar items it will show the corresponding view component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ClockView from './components/ClockView';
 import TimerView from './components/TimerView';
 import TimerSSNSview from './components/TimerSSNSview';
@@ -15,17 +15,20 @@ class App extends Component {
     
     return (
       <div className="container">
-        <div className='linkContainer'>
-          <Router>
+        <Router>
+          <div className='linkContainer'>
             <p className='indLink'><Link className='Link' to='/'>Clock</Link></p>
             <p className='indLink'><Link className='Link' to='/timer'>Timer</Link></p>
             <p className='indLink'><Link className='Link' to='/sessions'>Sessions</Link></p>
-          </Router>
-        </div>
-        
-        <TimerSSNSview />
-        {/* <TimerView /> */}
-        {/* <ClockView />  */}
+          </div>
+
+          <Routes>
+            <Route path='/' element={ <ClockView /> }/>
+            <Route path='/timer' element={ <TimerView /> }/>
+            <Route path='/sessions' element={ <TimerSSNSview /> }/>
+          </Routes>
+
+        </Router>
       </div>
     );
   }
